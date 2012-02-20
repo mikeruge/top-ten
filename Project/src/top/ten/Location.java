c893a6f021bb302ac7aa7340d205e278fb0c8659
mikeruge <michaelruge@my.unt.edu>
2012-02-19 18:10:46 -0600

    First section of project

---
+++ Project/src/top/ten/Location.java
@@ -0,0 +1,44 @@
+package top.ten;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class Location extends Activity 
+{
+	Button newButton;
+    Button curButton;
+
+	public OnClickListener secondListener = new OnClickListener()
+	{
+		@Override
+		public void onClick(View v)
+		{
+			if(v==curButton)
+			{
+				Intent newIntent = new Intent();
+				setResult(RESULT_OK, newIntent);
+                finish();
+			}
+			if(v==newButton)
+			{
+				Intent newIntent = new Intent();
+				setResult(RESULT_OK, newIntent);
+                finish();
+			}
+		}
+	};
+    @Override
+    public void onCreate(Bundle savedInstanceState) 
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main2);
+        newButton = (Button)findViewById(R.id.specificLoc);
+        curButton = (Button)findViewById(R.id.currentLoc);
+        newButton.setOnClickListener(secondListener);
+        curButton.setOnClickListener(secondListener);
+    }
+}
---
+++ Project/src/top/ten/TopTenActivity.java
@@ -0,0 +1,48 @@
+package top.ten;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class TopTenActivity extends Activity 
+{
+
+	Button prevButton;
+    Button topButton;
+    Button settingsButton;
+    Button exitButton;
+
+	public OnClickListener myListener = new OnClickListener()
+	{
+		@Override
+		public void onClick(View v)
+		{
+			if(v==topButton)
+			{
+				Intent newIntent = new Intent(v.getContext(), Location.class);
+				startActivityForResult(newIntent, 0);
+			}
+			if(v==exitButton)
+			{
+				finish();
+			}
+		}
+	};
+    @Override
+    public void onCreate(Bundle savedInstanceState) 
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        prevButton = (Button)findViewById(R.id.prevPlaces);
+        topButton = (Button)findViewById(R.id.topTen);
+        settingsButton = (Button)findViewById(R.id.mySettings);
+        exitButton = (Button)findViewById(R.id.theExit);
+        prevButton.setOnClickListener(myListener);
+        topButton.setOnClickListener(myListener);
+        settingsButton.setOnClickListener(myListener);
+        exitButton.setOnClickListener(myListener);
+    }
+}
